d16db5ec4c495ffdf670cfc667b273c7b214e591
Jae Han Song <rfv1479@gmail.com>
2023-02-16 16:37:39 +0900

    Feat #1 : 버튼 컴포넌트 제작 (Hierarchy 구성)
    - 1~5단계로 구성
    -props로 단계별 디자인 체계화

---
+++ frontend/src/Components/Button/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { ButtonProps } from './types';
+import useButtonHeirarchy from './Hooks/useHeirarchy';
+
+type ButtonReturn = (props: ButtonProps) => React.ReactElement;
+
+export const Button: ButtonReturn = (props: ButtonProps) => {
+  const { children, kind = 'button', href, type = 'button', step = 'first' } = props;
+  const commonStyle = 'w-full font-semibold px-10 py-3 shadow-md rounded-md';
+  const heirarchy = useButtonHeirarchy(step);
+  const style = `${commonStyle} ${heirarchy}`;
+
+  return (
+    <>
+      {kind === 'a' && (
+        <a className={style} href={href}>
+          {children}
+        </a>
+      )}
+      {kind === 'btn' && (
+        <button className={style} type={type}>
+          {children}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Button;
