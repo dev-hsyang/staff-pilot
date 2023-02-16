515420038893b639c2633c16b460f2bd86cb8730
Jae Han Song <rfv1479@gmail.com>
2023-02-16 13:44:45 +0900

    Feat #1 : 메인 페이지 및 config setting
    - eslint import order 추가
    - vite.config 수정
    - route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+import MainPage from '@/Pages/Main';
+import ErrorPage from '@/404';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((cnt) => cnt + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <>
+      <Routes key={location.pathname} location={location}>
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
 
