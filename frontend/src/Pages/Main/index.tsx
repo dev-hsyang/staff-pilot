fe6f429c0d0b42d43545bd7ac4dc992a1460a00f
Jae Han Song <rfv1479@gmail.com>
2023-02-16 14:57:33 +0900

    Feat #1 : 모바일/웹 레이아웃 css
     - viewport에 맞게 변경

--- frontend/src/Pages/Main/index.tsx
+++ frontend/src/Pages/Main/index.tsx
@@ -1,7 +1,18 @@
+import { MainPageWrapper } from './style';
+
 export default function MainPage() {
   return (
-    <>
-      <>하하</>
-    </>
+    <MainPageWrapper>
+      <h1>시프티에 오신 것을 환영합니다</h1>
+      <form id="login">
+        <a href="/account/register-form">
+          <button type="button">가입하기</button>
+        </a>
+        <a href="/account/login-form">
+          <button type="button">로그인하기</button>
+        </a>
+      </form>
+      <p>&copy; faketee2022</p>
+    </MainPageWrapper>
   );
 }
