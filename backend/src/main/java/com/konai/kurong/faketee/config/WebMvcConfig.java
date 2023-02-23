bf482d5bd41b9605c2a274a1d0f65f7f055e0016
hyunseung yang <dev.hsyang@gmail.com>
2023-02-23 21:34:27 +0900

    feat: Cross origin resource sharing 설정

--- backend/src/main/java/com/konai/kurong/faketee/config/WebMvcConfig.java
+++ backend/src/main/java/com/konai/kurong/faketee/config/WebMvcConfig.java
@@ -54,6 +54,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(empAuthInterceptor);
     }
 
+    /**
+     * CORS (Cross Over Resource Sharing) 을 위한 설정
+     *
+     * 프론트서버에서의 요청에대한 Response 의 헤더에 CORS 정보를 추가하여
+     * 외부 ORIGIN 에서 API 를 통한 데이터 송수신을 가능하게 한다.
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "https://faketee-8a8c9.web.app:5000", "https://faketee-8a8c9.web.app:5173")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+
     @Bean
     public RequestContextListener requestContextListener(){
 
