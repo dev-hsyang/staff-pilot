33474eaaae307873f21d651610436f1deb8676a6
hyunseung yang <dev.hsyang@gmail.com>
2023-05-22 21:00:20 +0900

    chore: 리눅스 환경에 맞게 welcome페이지 url 수정

--- backend/src/main/java/com/konai/kurong/faketee/utils/URI.java
+++ backend/src/main/java/com/konai/kurong/faketee/utils/URI.java
@@ -2,7 +2,7 @@ package com.konai.kurong.faketee.utils;
 
 public class URI {
 
-    public static final String INDEX_WELCOME_URI = "/index/welcome";
+    public static final String INDEX_WELCOME_URI = "index/welcome";
     public static final String INIT_USR_LOGIN_REDIRECT_URI = "/index/user-auth";
     public static final String USR_LOGIN_REDIRECT_URI = "/index/home-emp";
 
